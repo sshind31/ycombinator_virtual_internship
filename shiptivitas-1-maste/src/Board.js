8cff5a3baf73b9d980349ea5f43deb49730770d0
sshind31 <101928160+sshind31@users.noreply.github.com>
2023-08-29 11:28:52 -0700

    Update Board.js

--- shiptivitas-1-maste/src/Board.js
+++ shiptivitas-1-maste/src/Board.js
@@ -8,12 +8,12 @@ import axios from 'axios';
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
-    const clients = getClients();
+    const clients = [];//getClients();
     this.state = {
       clients: {
-        backlog: clients.filter(client => !client.status || client.status === 'backlog'),
-        inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
-        complete: clients.filter(client => client.status && client.status === 'complete'),
+        backlog: [],
+        inProgress: [],
+        complete: []
       }
     }
     this.swimlanes = {
@@ -23,44 +23,45 @@ export default class Board extends React.Component {
     }
   }
   async getClients() {
-    let res=await axios.get('http://localhost:3001/api/v1/clients',);
-      return res.data;}
-    // axios
-    //   .get('http://localhost:3001/api/v1/clients',)
-    //   .then((res) => {
-    //     console.log(res.data);
-    //     return res.data;
-    //   })
-    //   .catch((err) => {
-    //     console.log(err.message);
-    //   });
-    // return [
-    //   ['1', 'Stark, White and Abbott', 'Cloned Optimal Architecture', 'in-progress'],
-    //   ['2', 'Wiza LLC', 'Exclusive Bandwidth-Monitored Implementation', 'complete'],
-    //   ['3', 'Nolan LLC', 'Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-    //   ['4', 'Thompson PLC', 'Streamlined Regional Knowledgeuser', 'in-progress'],
-    //   ['5', 'Walker-Williamson', 'Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-    //   ['6', 'Boehm and Sons', 'Automated Systematic Paradigm', 'backlog'],
-    //   ['7', 'Runolfsson, Hegmann and Block', 'Integrated Transitional Strategy', 'backlog'],
-    //   ['8', 'Schumm-Labadie', 'Operative Heuristic Challenge', 'backlog'],
-    //   ['9', 'Kohler Group', 'Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-    //   ['10', 'Romaguera Inc', 'Managed Foreground Toolset', 'backlog'],
-    //   ['11', 'Reilly-King', 'Future-Proofed Interactive Toolset', 'complete'],
-    //   ['12', 'Emard, Champlin and Runolfsdottir', 'Devolved Needs-Based Capability', 'backlog'],
-    //   ['13', 'Fritsch, Cronin and Wolff', 'Open-Source 3Rdgeneration Website', 'complete'],
-    //   ['14', 'Borer LLC', 'Profit-Focused Incremental Orchestration', 'backlog'],
-    //   ['15', 'Emmerich-Ankunding', 'User-Centric Stable Extranet', 'in-progress'],
-    //   ['16', 'Willms-Abbott', 'Progressive Bandwidth-Monitored Access', 'in-progress'],
-    //   ['17', 'Brekke PLC', 'Intuitive User-Facing Customerloyalty', 'complete'],
-    //   ['18', 'Bins, Toy and Klocko', 'Integrated Assymetric Software', 'backlog'],
-    //   ['19', 'Hodkiewicz-Hayes', 'Programmable Systematic Securedline', 'backlog'],
-    //   ['20', 'Murphy, Lang and Ferry', 'Organized Explicit Access', 'backlog'],
-    // ].map(companyDetails => ({
-    //   id: companyDetails[0],
-    //   name: companyDetails[1],
-    //   description: companyDetails[2],
-    //   status: companyDetails[3],
-    // }));
+    let res = await axios.get('http://localhost:3001/api/v1/clients',);
+    return res.data;
+  }
+  // axios
+  //   .get('http://localhost:3001/api/v1/clients',)
+  //   .then((res) => {
+  //     console.log(res.data);
+  //     return res.data;
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.message);
+  //   });
+  // return [
+  //   ['1', 'Stark, White and Abbott', 'Cloned Optimal Architecture', 'in-progress'],
+  //   ['2', 'Wiza LLC', 'Exclusive Bandwidth-Monitored Implementation', 'complete'],
+  //   ['3', 'Nolan LLC', 'Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
+  //   ['4', 'Thompson PLC', 'Streamlined Regional Knowledgeuser', 'in-progress'],
+  //   ['5', 'Walker-Williamson', 'Team-Oriented 6Thgeneration Matrix', 'in-progress'],
+  //   ['6', 'Boehm and Sons', 'Automated Systematic Paradigm', 'backlog'],
+  //   ['7', 'Runolfsson, Hegmann and Block', 'Integrated Transitional Strategy', 'backlog'],
+  //   ['8', 'Schumm-Labadie', 'Operative Heuristic Challenge', 'backlog'],
+  //   ['9', 'Kohler Group', 'Re-Contextualized Multi-Tasking Attitude', 'backlog'],
+  //   ['10', 'Romaguera Inc', 'Managed Foreground Toolset', 'backlog'],
+  //   ['11', 'Reilly-King', 'Future-Proofed Interactive Toolset', 'complete'],
+  //   ['12', 'Emard, Champlin and Runolfsdottir', 'Devolved Needs-Based Capability', 'backlog'],
+  //   ['13', 'Fritsch, Cronin and Wolff', 'Open-Source 3Rdgeneration Website', 'complete'],
+  //   ['14', 'Borer LLC', 'Profit-Focused Incremental Orchestration', 'backlog'],
+  //   ['15', 'Emmerich-Ankunding', 'User-Centric Stable Extranet', 'in-progress'],
+  //   ['16', 'Willms-Abbott', 'Progressive Bandwidth-Monitored Access', 'in-progress'],
+  //   ['17', 'Brekke PLC', 'Intuitive User-Facing Customerloyalty', 'complete'],
+  //   ['18', 'Bins, Toy and Klocko', 'Integrated Assymetric Software', 'backlog'],
+  //   ['19', 'Hodkiewicz-Hayes', 'Programmable Systematic Securedline', 'backlog'],
+  //   ['20', 'Murphy, Lang and Ferry', 'Organized Explicit Access', 'backlog'],
+  // ].map(companyDetails => ({
+  //   id: companyDetails[0],
+  //   name: companyDetails[1],
+  //   description: companyDetails[2],
+  //   status: companyDetails[3],
+  // }));
   //}
   renderSwimlane(name, clients, ref) {
     return (
@@ -88,6 +89,18 @@ export default class Board extends React.Component {
     );
   }
   componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/clients',).then(res => {
+      this.setState({
+        clients:
+        {
+          backlog: res.data.filter(client => !client.status || client.status === 'backlog'),
+          inProgress: res.data.filter(client => client.status && client.status === 'in-progress'),
+          complete: res.data.filter(client => client.status && client.status === 'complete'),
+        }
+      })
+    });
+    // this.setState({ clients: reqData.data })
+
     this.drake = Dragula([
       this.swimlanes.backlog.current,
       this.swimlanes.inProgress.current,
