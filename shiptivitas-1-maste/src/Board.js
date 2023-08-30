52523579829efcdf85abc9960db47ce4e733c45c
sshind31 <101928160+sshind31@users.noreply.github.com>
2023-08-30 12:46:06 +0530

    Update Board.js

--- shiptivitas-1-maste/src/Board.js
+++ shiptivitas-1-maste/src/Board.js
@@ -8,7 +8,15 @@ import axios from 'axios';
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
-    const clients = [];//getClients();
+    // const clients = this.getClients();
+    // this.state = {
+    //   clients: {
+    //     backlog: clients.filter(client => !client.status || client.status === 'backlog'),
+    //     inProgress: clients.filter(client => client.status && client.status === 'in-progress'),
+    //     complete: clients.filter(client => client.status && client.status === 'complete'),
+    //   }
+    // }
+    const clients = [];
     this.state = {
       clients: {
         backlog: [],
@@ -22,47 +30,35 @@ export default class Board extends React.Component {
       complete: React.createRef(),
     }
   }
-  async getClients() {
-    let res = await axios.get('http://localhost:3001/api/v1/clients',);
-    return res.data;
+  getClients() {
+    return [
+      ['1', 'Stark, White and Abbott', 'Cloned Optimal Architecture', 'in-progress'],
+      ['2', 'Wiza LLC', 'Exclusive Bandwidth-Monitored Implementation', 'complete'],
+      ['3', 'Nolan LLC', 'Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
+      ['4', 'Thompson PLC', 'Streamlined Regional Knowledgeuser', 'in-progress'],
+      ['5', 'Walker-Williamson', 'Team-Oriented 6Thgeneration Matrix', 'in-progress'],
+      ['6', 'Boehm and Sons', 'Automated Systematic Paradigm', 'backlog'],
+      ['7', 'Runolfsson, Hegmann and Block', 'Integrated Transitional Strategy', 'backlog'],
+      ['8', 'Schumm-Labadie', 'Operative Heuristic Challenge', 'backlog'],
+      ['9', 'Kohler Group', 'Re-Contextualized Multi-Tasking Attitude', 'backlog'],
+      ['10', 'Romaguera Inc', 'Managed Foreground Toolset', 'backlog'],
+      ['11', 'Reilly-King', 'Future-Proofed Interactive Toolset', 'complete'],
+      ['12', 'Emard, Champlin and Runolfsdottir', 'Devolved Needs-Based Capability', 'backlog'],
+      ['13', 'Fritsch, Cronin and Wolff', 'Open-Source 3Rdgeneration Website', 'complete'],
+      ['14', 'Borer LLC', 'Profit-Focused Incremental Orchestration', 'backlog'],
+      ['15', 'Emmerich-Ankunding', 'User-Centric Stable Extranet', 'in-progress'],
+      ['16', 'Willms-Abbott', 'Progressive Bandwidth-Monitored Access', 'in-progress'],
+      ['17', 'Brekke PLC', 'Intuitive User-Facing Customerloyalty', 'complete'],
+      ['18', 'Bins, Toy and Klocko', 'Integrated Assymetric Software', 'backlog'],
+      ['19', 'Hodkiewicz-Hayes', 'Programmable Systematic Securedline', 'backlog'],
+      ['20', 'Murphy, Lang and Ferry', 'Organized Explicit Access', 'backlog'],
+    ].map(companyDetails => ({
+      id: companyDetails[0],
+      name: companyDetails[1],
+      description: companyDetails[2],
+      status: companyDetails[3],
+    }));
   }
-  // axios
-  //   .get('http://localhost:3001/api/v1/clients',)
-  //   .then((res) => {
-  //     console.log(res.data);
-  //     return res.data;
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.message);
-  //   });
-  // return [
-  //   ['1', 'Stark, White and Abbott', 'Cloned Optimal Architecture', 'in-progress'],
-  //   ['2', 'Wiza LLC', 'Exclusive Bandwidth-Monitored Implementation', 'complete'],
-  //   ['3', 'Nolan LLC', 'Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-  //   ['4', 'Thompson PLC', 'Streamlined Regional Knowledgeuser', 'in-progress'],
-  //   ['5', 'Walker-Williamson', 'Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-  //   ['6', 'Boehm and Sons', 'Automated Systematic Paradigm', 'backlog'],
-  //   ['7', 'Runolfsson, Hegmann and Block', 'Integrated Transitional Strategy', 'backlog'],
-  //   ['8', 'Schumm-Labadie', 'Operative Heuristic Challenge', 'backlog'],
-  //   ['9', 'Kohler Group', 'Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-  //   ['10', 'Romaguera Inc', 'Managed Foreground Toolset', 'backlog'],
-  //   ['11', 'Reilly-King', 'Future-Proofed Interactive Toolset', 'complete'],
-  //   ['12', 'Emard, Champlin and Runolfsdottir', 'Devolved Needs-Based Capability', 'backlog'],
-  //   ['13', 'Fritsch, Cronin and Wolff', 'Open-Source 3Rdgeneration Website', 'complete'],
-  //   ['14', 'Borer LLC', 'Profit-Focused Incremental Orchestration', 'backlog'],
-  //   ['15', 'Emmerich-Ankunding', 'User-Centric Stable Extranet', 'in-progress'],
-  //   ['16', 'Willms-Abbott', 'Progressive Bandwidth-Monitored Access', 'in-progress'],
-  //   ['17', 'Brekke PLC', 'Intuitive User-Facing Customerloyalty', 'complete'],
-  //   ['18', 'Bins, Toy and Klocko', 'Integrated Assymetric Software', 'backlog'],
-  //   ['19', 'Hodkiewicz-Hayes', 'Programmable Systematic Securedline', 'backlog'],
-  //   ['20', 'Murphy, Lang and Ferry', 'Organized Explicit Access', 'backlog'],
-  // ].map(companyDetails => ({
-  //   id: companyDetails[0],
-  //   name: companyDetails[1],
-  //   description: companyDetails[2],
-  //   status: companyDetails[3],
-  // }));
-  //}
   renderSwimlane(name, clients, ref) {
     return (
       <Swimlane name={name} clients={clients} dragulaRef={ref} />
@@ -99,7 +95,6 @@ export default class Board extends React.Component {
         }
       })
     });
-    // this.setState({ clients: reqData.data })
 
     this.drake = Dragula([
       this.swimlanes.backlog.current,
@@ -134,13 +129,13 @@ export default class Board extends React.Component {
       ...this.state.clients.inProgress,
       ...this.state.clients.complete,
     ];
-    const clientThatMoved = clientsList.find(client => client.id === el.dataset.id);
+    const clientThatMoved = clientsList.find(client => client.id == el.dataset.id);
     console.log(clientThatMoved.status + " $$ " + clientThatMoved.name);
     const clientThatMovedClone = {
       ...clientThatMoved,
       status: targetSwimlane,
     };
-    console.log(clientThatMovedClone.status + " $$ " + clientThatMovedClone.name + " $$ " + clientThatMovedClone);
+    console.log(clientThatMovedClone.status + " $$ " + clientThatMovedClone.name + " $$ " + clientThatMovedClone.id);
 
     console.log('http://localhost:3001/api/v1/clients/:{' + clientThatMoved.id + '}');
 
