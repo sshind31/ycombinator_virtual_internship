4e808056d70204991350430f37e0a8a6a5c36e5c
sshind31 <101928160+sshind31@users.noreply.github.com>
2023-08-30 17:51:00 +0530

    Update Board.js

--- shiptivitas-1-maste/src/Board.js
+++ shiptivitas-1-maste/src/Board.js
@@ -16,7 +16,6 @@ export default class Board extends React.Component {
     //     complete: clients.filter(client => client.status && client.status === 'complete'),
     //   }
     // }
-    const clients = [];
     this.state = {
       clients: {
         backlog: [],
@@ -30,35 +29,35 @@ export default class Board extends React.Component {
       complete: React.createRef(),
     }
   }
-  getClients() {
-    return [
-      ['1', 'Stark, White and Abbott', 'Cloned Optimal Architecture', 'in-progress'],
-      ['2', 'Wiza LLC', 'Exclusive Bandwidth-Monitored Implementation', 'complete'],
-      ['3', 'Nolan LLC', 'Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
-      ['4', 'Thompson PLC', 'Streamlined Regional Knowledgeuser', 'in-progress'],
-      ['5', 'Walker-Williamson', 'Team-Oriented 6Thgeneration Matrix', 'in-progress'],
-      ['6', 'Boehm and Sons', 'Automated Systematic Paradigm', 'backlog'],
-      ['7', 'Runolfsson, Hegmann and Block', 'Integrated Transitional Strategy', 'backlog'],
-      ['8', 'Schumm-Labadie', 'Operative Heuristic Challenge', 'backlog'],
-      ['9', 'Kohler Group', 'Re-Contextualized Multi-Tasking Attitude', 'backlog'],
-      ['10', 'Romaguera Inc', 'Managed Foreground Toolset', 'backlog'],
-      ['11', 'Reilly-King', 'Future-Proofed Interactive Toolset', 'complete'],
-      ['12', 'Emard, Champlin and Runolfsdottir', 'Devolved Needs-Based Capability', 'backlog'],
-      ['13', 'Fritsch, Cronin and Wolff', 'Open-Source 3Rdgeneration Website', 'complete'],
-      ['14', 'Borer LLC', 'Profit-Focused Incremental Orchestration', 'backlog'],
-      ['15', 'Emmerich-Ankunding', 'User-Centric Stable Extranet', 'in-progress'],
-      ['16', 'Willms-Abbott', 'Progressive Bandwidth-Monitored Access', 'in-progress'],
-      ['17', 'Brekke PLC', 'Intuitive User-Facing Customerloyalty', 'complete'],
-      ['18', 'Bins, Toy and Klocko', 'Integrated Assymetric Software', 'backlog'],
-      ['19', 'Hodkiewicz-Hayes', 'Programmable Systematic Securedline', 'backlog'],
-      ['20', 'Murphy, Lang and Ferry', 'Organized Explicit Access', 'backlog'],
-    ].map(companyDetails => ({
-      id: companyDetails[0],
-      name: companyDetails[1],
-      description: companyDetails[2],
-      status: companyDetails[3],
-    }));
-  }
+  // getClients() {
+  //   return [
+  //     ['1', 'Stark, White and Abbott', 'Cloned Optimal Architecture', 'in-progress'],
+  //     ['2', 'Wiza LLC', 'Exclusive Bandwidth-Monitored Implementation', 'complete'],
+  //     ['3', 'Nolan LLC', 'Vision-Oriented 4Thgeneration Graphicaluserinterface', 'backlog'],
+  //     ['4', 'Thompson PLC', 'Streamlined Regional Knowledgeuser', 'in-progress'],
+  //     ['5', 'Walker-Williamson', 'Team-Oriented 6Thgeneration Matrix', 'in-progress'],
+  //     ['6', 'Boehm and Sons', 'Automated Systematic Paradigm', 'backlog'],
+  //     ['7', 'Runolfsson, Hegmann and Block', 'Integrated Transitional Strategy', 'backlog'],
+  //     ['8', 'Schumm-Labadie', 'Operative Heuristic Challenge', 'backlog'],
+  //     ['9', 'Kohler Group', 'Re-Contextualized Multi-Tasking Attitude', 'backlog'],
+  //     ['10', 'Romaguera Inc', 'Managed Foreground Toolset', 'backlog'],
+  //     ['11', 'Reilly-King', 'Future-Proofed Interactive Toolset', 'complete'],
+  //     ['12', 'Emard, Champlin and Runolfsdottir', 'Devolved Needs-Based Capability', 'backlog'],
+  //     ['13', 'Fritsch, Cronin and Wolff', 'Open-Source 3Rdgeneration Website', 'complete'],
+  //     ['14', 'Borer LLC', 'Profit-Focused Incremental Orchestration', 'backlog'],
+  //     ['15', 'Emmerich-Ankunding', 'User-Centric Stable Extranet', 'in-progress'],
+  //     ['16', 'Willms-Abbott', 'Progressive Bandwidth-Monitored Access', 'in-progress'],
+  //     ['17', 'Brekke PLC', 'Intuitive User-Facing Customerloyalty', 'complete'],
+  //     ['18', 'Bins, Toy and Klocko', 'Integrated Assymetric Software', 'backlog'],
+  //     ['19', 'Hodkiewicz-Hayes', 'Programmable Systematic Securedline', 'backlog'],
+  //     ['20', 'Murphy, Lang and Ferry', 'Organized Explicit Access', 'backlog'],
+  //   ].map(companyDetails => ({
+  //     id: companyDetails[0],
+  //     name: companyDetails[1],
+  //     description: companyDetails[2],
+  //     status: companyDetails[3],
+  //   }));
+  // }
   renderSwimlane(name, clients, ref) {
     return (
       <Swimlane name={name} clients={clients} dragulaRef={ref} />
@@ -89,9 +88,9 @@ export default class Board extends React.Component {
       this.setState({
         clients:
         {
-          backlog: res.data.filter(client => !client.status || client.status === 'backlog'),
-          inProgress: res.data.filter(client => client.status && client.status === 'in-progress'),
-          complete: res.data.filter(client => client.status && client.status === 'complete'),
+          backlog: res.data.filter(client => !client.status || client.status === 'backlog').sort(function(a,b){return a.priority-b.priority}),
+          inProgress: res.data.filter(client => client.status && client.status === 'in-progress').sort(function(a,b){return a.priority-b.priority}),
+          complete: res.data.filter(client => client.status && client.status === 'complete').sort(function(a,b){return a.priority-b.priority}),
         }
       })
     });
@@ -143,13 +142,14 @@ export default class Board extends React.Component {
     const updatedClients = clientsList.filter(client => client.id !== clientThatMovedClone.id);
     // Place ClientThatMoved just before the sibling client, keeping the order
     const index = updatedClients.findIndex(client => sibling && client.id === sibling.dataset.id);
+    
     updatedClients.splice(index === -1 ? updatedClients.length : index, 0, clientThatMovedClone);
     // Update React state to reflect changes
     this.setState({
       clients: {
-        backlog: updatedClients.filter(client => !client.status || client.status === 'backlog'),
-        inProgress: updatedClients.filter(client => client.status && client.status === 'in-progress'),
-        complete: updatedClients.filter(client => client.status && client.status === 'complete'),
+        backlog: updatedClients.filter(client => !client.status || client.status === 'backlog').sort(function(a,b){return a.priority-b.priority}),
+        inProgress: updatedClients.filter(client => client.status && client.status === 'in-progress').sort(function(a,b){return a.priority-b.priority}),
+        complete: updatedClients.filter(client => client.status && client.status === 'complete').sort(function(a,b){return a.priority-b.priority}),
       }
     });
     const payload = {
